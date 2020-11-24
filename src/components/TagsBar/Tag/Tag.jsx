16e4c4aa0a60a70eacc8d735127938d6ee35917a
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 21:57:26 +0100

    Added full structure to Tag component

--- src/components/TagsBar/Tag/Tag.jsx
+++ src/components/TagsBar/Tag/Tag.jsx
@@ -4,8 +4,15 @@ import "./Tag.css";
 function Tag({title}) {
     return (
         <div className="tag">
-            <h4>{title}</h4>
-        </div>)
+            <h5 className="tag__title">{title}</h5>
+            <button className="tag__delete-btn">
+                <img className="tag__delete-btn-image" 
+                    src={`${process.env.PUBLIC_URL}/images/icon-remove.svg`} 
+                    alt="Remove icon"
+                />
+            </button>
+        </div>
+    )
 
 }
 
