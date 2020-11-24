4a00cef154e2b9c41656704c6a9d0a19f63e2af4
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 22:01:48 +0100

    Added component for displaying jobs

---
+++ src/components/Jobs/Card/JobCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./JobCard.css";
+
+function JobCard() {
+    return (
+        <div className="job-card">
+            
+        </div>
+    )
+}
+
+export default JobCard
