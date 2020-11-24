9c2c87ce69d8711a585f2e4fe5db3d27cfcd3977
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 23:23:14 +0100

    Created job card footer for additional info

---
+++ src/components/Jobs/JobCardFooter/JobCardFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./JobCardFooter.css";
+
+function JobCardFooter() {
+    return (
+        <div className="job-card-footer">
+            
+        </div>
+    )
+}
+
+export default JobCardFooter
