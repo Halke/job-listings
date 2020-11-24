c1b9280414da22b5c4400063eaa9286da73f3ad9
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 23:19:30 +0100

    Added job card header to job card component

--- src/components/Jobs/Card/JobCard.jsx
+++ src/components/Jobs/Card/JobCard.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./JobCard.css";
+import "../JobCardHeader/JobCardHeader";
+import JobCardHeader from '../JobCardHeader/JobCardHeader';
 
-function JobCard() {
+function JobCard({logoUrl, companyName, newPost, featuredPost}) {
     return (
         <div className="job-card">
-            
+            <img className="job-card__image" 
+                src={logoUrl} 
+                alt="Company logo" 
+            />
+            <div className="job-card__description">
+                <div className="job-card__description-head">
+                    <JobCardHeader 
+                        companyName={companyName} 
+                        newPost={newPost}
+                        featuredPost={featuredPost}
+                    />
+                </div>
+                <div className="job-card__description-job-title">
+
+                </div>
+            </div>
+            <div className="job-card__tags">
+
+            </div>
         </div>
     )
 }
