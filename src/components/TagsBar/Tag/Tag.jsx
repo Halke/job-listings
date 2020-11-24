c114160fd72a988902a3ff15844f1818cb355514
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 22:14:23 +0100

    Modified structure to make comp reusable

--- src/components/TagsBar/Tag/Tag.jsx
+++ src/components/TagsBar/Tag/Tag.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import "./Tag.css";
 
-function Tag({title}) {
+function Tag({title, removeable=true}) {
     return (
         <div className="tag">
             <h5 className="tag__title">{title}</h5>
-            <button className="tag__delete-btn">
+            {removeable ? 
+            (
+                <button className="tag__delete-btn">
                 <img className="tag__delete-btn-image" 
                     src={`${process.env.PUBLIC_URL}/images/icon-remove.svg`} 
                     alt="Remove icon"
                 />
             </button>
+            )
+            :
+            null}
         </div>
     )
 
