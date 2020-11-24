9f79f29bc57c07f444fbf23923763b44e32c9033
Dino Halilovic <Dino.primary@gmail.com>
2020-11-24 16:01:17 +0100

    Added background to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,10 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            
+            <img className="header__image"
+                src={`${process.env.PUBLIC_URL}/images/bg-header-desktop.svg`} 
+                alt="Header banner" 
+            />
         </div>
     )
 }
