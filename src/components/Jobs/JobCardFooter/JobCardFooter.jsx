1d9d67461edffa1d736b568c04ebb8f9fdf1702b
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 09:45:07 +0100

    Added destructured props to component

--- src/components/Jobs/JobCardFooter/JobCardFooter.jsx
+++ src/components/Jobs/JobCardFooter/JobCardFooter.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./JobCardFooter.css";
 
-function JobCardFooter() {
+function JobCardFooter({postedAt, contract, location}) {
     return (
         <div className="job-card-footer">
-            
+            <h6>{postedAt}</h6>
+            <h6>&#183;</h6>
+            <h6>{contract}</h6>
+            <h6>&#183;</h6>
+            <h6>{location}</h6>
         </div>
     )
 }
