33c452c32a4d6b66fdc9fa8f66bd87badb310a17
Dino Halilovic <Dino.primary@gmail.com>
2020-11-26 00:25:55 +0100

    Added Tags context provider to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import './App.css';
 import Page from "./components/Jobs/Page/Page";
+import TagsContext from "./components/context/TagsContext";
 
 function App() {
+
+  const Context = useContext(TagsContext);
+
+  const [tags, setTags] = useState([]);
+
+  const addTag = (tag) => {
+    setTags(prevTags => [...prevTags, tag]);
+  }
+
+  const removeTag = (tag) => {
+    setTags(prevTags => {
+      return prevTags.filter(t => t !== tag);
+    });
+  }
+
+  const setToDefault = () => {
+    setTags([]);
+  }
+
   return (
     <div className="App">
-      <Page />
+      <Context.Provider>
+        <Page value={{tags, addTag, removeTag, setToDefault}} />
+      </Context.Provider>
     </div>
   );
 }
