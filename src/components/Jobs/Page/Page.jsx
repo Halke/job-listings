8007860bd14e2ad90d76e89e91f3d7fa92746c4c
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 23:32:38 +0100

    Added state update when data is fetched

--- src/components/Jobs/Page/Page.jsx
+++ src/components/Jobs/Page/Page.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from "../../Header/Header";
 import JobCard from "../Card/JobCard";
 import Bar from "../../TagsBar/Bar/Bar";
 import "./Page.css";
 
 function Page() {
+
+    //const jsonFilePath = "data.json";
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch("data.json", {
+            headers : { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+           }
+        })
+        .then(res => res.json())
+        .then(data => setJobs(data))
+        .catch(err => console.log("ERROR: ", err))
+    }, []);
+
     return (
         <div className="page">
             <Header />
             <div className="page__bar-section">
                 <Bar />
             </div>
+            
             <JobCard 
                 companyName="Photosnap"
                 newPost={true}
