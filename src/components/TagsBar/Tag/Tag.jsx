6b4bd27af6a3d68a1ef74689a23b92c4a7276239
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 13:16:20 +0100

    Setted remove icon as delete btn bg

--- src/components/TagsBar/Tag/Tag.jsx
+++ src/components/TagsBar/Tag/Tag.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import "./Tag.css";
 
-function Tag({title, removeable=true, className}) {
+function Tag({title, removeable=true, className="", isDisabled}) {
+
+    const handleClick = (e) => {
+        console.log(e.target.id);
+    }
+
+    const handleDelete = (e) => {
+        console.log(e.target.id);
+    }
 
     return (
         <div className={"tag " + className}>
-            <h5 className="tag__title">
+            {/* <h5 className="tag__title">
                 {title}
-            </h5>
+            </h5> */}
+            <button disabled={isDisabled ? true : false} 
+                id={isDisabled ? null : title} 
+                onClick={handleClick} 
+                className="tag__title"
+            >
+                {title}
+            </button>
             {removeable ? 
             (
             <button
+                id={title}
+                onClick={handleDelete}
                 className="tag__delete-btn"
             >
-                <img className="tag__delete-btn-image" 
-                    src={`${process.env.PUBLIC_URL}/images/icon-remove.svg`} 
+                {/* <img className="tag__delete-btn-image" 
+                    src={"/src/images/icon-remove.svg"}
                     alt="Remove icon"
-                />
+                /> */}
             </button>
             )
             :
