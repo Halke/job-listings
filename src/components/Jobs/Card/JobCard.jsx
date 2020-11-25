105015c79b6126fee566773f34d54270695fdeeb
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 18:39:43 +0100

    Added Image component to job card

--- src/components/Jobs/Card/JobCard.jsx
+++ src/components/Jobs/Card/JobCard.jsx
@@ -4,15 +4,21 @@ import "../JobCardHeader/JobCardHeader";
 import JobCardHeader from '../JobCardHeader/JobCardHeader';
 import JobCardFooter from "../JobCardFooter/JobCardFooter";
 import Tag from "../../TagsBar/Tag/Tag";
+import Image from "../Image/Image";
+//import logo from "../../../images/photosnap.svg";
 
-function JobCard({logoUrl, companyName, newPost, featuredPost, position, tags}) {
+function JobCard({logoSource, companyName, newPost, featuredPost, position, tags}) {
+    
+    //const logo = require("../../../images/" + logoSource);
+
     return (
         <div className="job-card">
             <div className="job-card__description">
-                <img className="job-card__description-image" 
-                    src={logoUrl} 
+                {/* <img className="job-card__description-image" 
+                    src={require("../../../images/" + logoSource)} 
                     alt="Company logo" 
-                />
+                /> */}
+                <Image companyName="Photosnap" />
                 <div className="job-card__description-info">
                     <JobCardHeader 
                         companyName={companyName} 
@@ -25,7 +31,7 @@ function JobCard({logoUrl, companyName, newPost, featuredPost, position, tags}) 
                         </h2>
                     </div>
                     <JobCardFooter 
-                        postedAt="5 Days ago"
+                        postedAt="5D ago"
                         contract="Part time"
                         location="Europe"
                     />
@@ -33,7 +39,8 @@ function JobCard({logoUrl, companyName, newPost, featuredPost, position, tags}) 
             </div>
             <div className="job-card__tags">
                 {tags.map((tag, index) => (
-                    <Tag 
+                    <Tag
+                        isDisabled={false}
                         className="job-card__tags-tag"
                         key={index} title={tag} 
                         removeable={false}
