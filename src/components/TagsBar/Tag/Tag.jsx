2c206f4f33768ecdcd47b197925d5f933d659e64
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 10:18:59 +0100

    Added className to desturctured props

--- src/components/TagsBar/Tag/Tag.jsx
+++ src/components/TagsBar/Tag/Tag.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import "./Tag.css";
 
-function Tag({title, removeable=true}) {
+function Tag({title, removeable=true, className}) {
+
     return (
-        <div className="tag">
-            <h5 className="tag__title">{title}</h5>
+        <div className={"tag " + className}>
+            <h5 className="tag__title">
+                {title}
+            </h5>
             {removeable ? 
             (
-                <button className="tag__delete-btn">
+            <button
+                className="tag__delete-btn"
+            >
                 <img className="tag__delete-btn-image" 
                     src={`${process.env.PUBLIC_URL}/images/icon-remove.svg`} 
                     alt="Remove icon"
