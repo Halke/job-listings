fadca17ca49f81c00eb3af335cf1e335ab6c0d51
Dino Halilovic <Dino.primary@gmail.com>
2020-11-25 11:13:14 +0100

    Completed Job Card Footer

--- src/components/Jobs/JobCardFooter/JobCardFooter.jsx
+++ src/components/Jobs/JobCardFooter/JobCardFooter.jsx
@@ -5,9 +5,9 @@ function JobCardFooter({postedAt, contract, location}) {
     return (
         <div className="job-card-footer">
             <h6>{postedAt}</h6>
-            <h6>&#183;</h6>
+            <h6 className="job-card-footer__dot-symbol">&#183;</h6>
             <h6>{contract}</h6>
-            <h6>&#183;</h6>
+            <h6 className="job-card-footer__dot-symbol">&#183;</h6>
             <h6>{location}</h6>
         </div>
     )
