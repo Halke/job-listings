039fa6cd15d714593db32d17acf627e41650ddde
Dino Halilovic <Dino.primary@gmail.com>
2020-11-26 11:04:02 +0100

    Modified fade in style rule

--- src/components/TagsBar/Bar/Bar.jsx
+++ src/components/TagsBar/Bar/Bar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, {useContext} from 'react'
 import "./Bar.css";
 import Tag from "../Tag/Tag";
 import TagsContext from "../../context/TagsContext";
