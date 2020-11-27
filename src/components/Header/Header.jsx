542f6877e7b360b0edd24d63cf46fb5d23cf9152
Dino Halilovic <Dino.primary@gmail.com>
2020-11-27 10:41:01 +0100

    Made image rendering dependant of device type

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./Header.css";
-import Banner from "../../images/bg-header-desktop.svg";
+import BannerDesktop from "../../images/bg-header-desktop.svg";
+import BannerMobile from "../../images/bg-header-mobile.svg";
+import {isMobile} from "react-device-detect";
 
 
 function Header() {
     return (
         <div className="header">
+            
             <img className="header__image"
-                src={Banner} 
+                src={isMobile ? BannerMobile : BannerDesktop} 
                 alt="Header banner" 
-            />
+            /> 
+            
         </div>
     )
 }
